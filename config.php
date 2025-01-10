1e52ea574f7d21929e0539b79ac2b7f4d81d0cff
Rali <ralica.a.simova@gmail.com>
2025-01-10 19:39:17 +0200

    Add config

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+class Config
+{
+    function __construct($filePath)
+    {
+        if (!file_exists($filePath)) {
+            throw new Exception("The .env file does not exist at {$filePath}");
+        }
+
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            if (str_starts_with(trim($line), '#')) {
+                continue;
+            }
+
+            [$key, $value] = explode('=', $line, 2);
+
+            $key = trim($key);
+            $value = trim($value);
+            $value = trim($value, "'\""); // Remove quotes around the value
+
+            $_ENV[$key] = $value;
+            putenv("{$key}={$value}");
+        }
+    }
+
+}
+
+$config = new Config(__DIR__ . '/.env');
