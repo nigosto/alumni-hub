061e1e522b7e236a37e89fb440b90ee14bf185da
rosenkolev1 <50500415+rosenkolev1@users.noreply.github.com>
2025-01-11 13:38:05 +0200

    Create rest of database tables and rollbacks

--- database/migrations/create_tables/migrate.php
+++ database/migrations/create_tables/migrate.php
@@ -4,16 +4,17 @@ require_once __DIR__ . "/../../index.php";
 $db_name = $_ENV['DB_NAME'];
 $db_con = $database->connection();
 
-$db_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
-$db_con->query(<<<CT
+$db_con->exec(<<<CT
 CREATE TABLE IF NOT EXISTS Users (
     id INT NOT NULL auto_increment PRIMARY KEY,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(30),
     username VARCHAR(50) UNIQUE NOT NULL,
-    role ENUM('Student', 'Administrator', 'Admin') NOT NULL
+    role ENUM('student', 'administrator', 'admin') NOT NULL
 );
+CT);
 
+$db_con->exec(<<<CT
 CREATE TABLE IF NOT EXISTS Students (
     fn VARCHAR(10) NOT NULL PRIMARY KEY,
     degree ENUM('bachelor', 'master', 'doctor') NOT NULL,
@@ -21,13 +22,43 @@ CREATE TABLE IF NOT EXISTS Students (
     graduation_year INT NOT NULL,
     grade DOUBLE NOT NULL,
     user_id INT,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES Users(id)
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
+CT);
 
+$db_con->exec(<<<CT
 CREATE TABLE IF NOT EXISTS Clothes (
     id INT NOT NULL auto_increment PRIMARY KEY,
     student_fn VARCHAR(10),
-    CONSTRAINT fk_student_fn FOREIGN KEY (student_fn) REFERENCES Students(fn)
+    FOREIGN KEY (student_fn) REFERENCES Students(fn)
+);
+CT);
+
+$db_con->exec(<<<CT
+CREATE TABLE IF NOT EXISTS Ceremony (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    date DATETIME NOT NULL
+);
+CT);
+
+$db_con->exec(<<<CT
+CREATE TABLE IF NOT EXISTS Ceremony_Attendance (
+    ceremony_id INT NOT NULL,
+    FOREIGN KEY (ceremony_id) REFERENCES Ceremony(id),
+
+    student_fn VARCHAR(10) NOT NULL,
+    FOREIGN KEY (student_fn) REFERENCES Students(fn),
+
+    PRIMARY KEY (ceremony_id, student_fn),
+
+    accepted BOOLEAN,
+    speach_status ENUM('none', 'waiting', 'declined', 'accepted') NOT NULL,
+
+    boss_status ENUM(
+    'none', 
+    'waiting_robes', 'waiting_signatures', 'waiting_diplomas', 
+    'accepted_robes', 'accepted_signatures', 'accepted_diplomas',
+    'declined_robes', 'declined_signatures', 'declined_diplomas') 
 );
 CT);
 
--- database/migrations/create_tables/rollback.php
+++ database/migrations/create_tables/rollback.php
@@ -4,10 +4,24 @@ require_once __DIR__ . "/../../index.php";
 $db_name = $_ENV['DB_NAME'];
 $db_con = $database->connection();
 
-$db_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db_con->exec(<<<CT
+DROP TABLE IF EXISTS Ceremony_Attendance;
+CT);
 
-// Drop the tables
-$db_con->exec("DROP TABLE IF EXISTS Clothes");
-$db_con->exec("DROP TABLE IF EXISTS Students");
-$db_con->exec("DROP TABLE IF EXISTS Users");
+$db_con->exec(<<<CT
+DROP TABLE IF EXISTS Clothes;
+CT);
+
+$db_con->exec(<<<CT
+DROP TABLE IF EXISTS Students;
+CT);
+
+$db_con->exec(<<<CT
+DROP TABLE IF EXISTS Users;
+CT);
+
+$db_con->exec(<<<CT
+DROP TABLE IF EXISTS Ceremony;
+CT);
+
 ?>
