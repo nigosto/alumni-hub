acfeaf70a4989d3da160ed748a17d0967be6f77c
rosenkolev1 <50500415+rosenkolev1@users.noreply.github.com>
2025-01-11 15:59:07 +0200

    Fix rollback multiline strings

--- database/migrations/create_tables/rollback.php
+++ database/migrations/create_tables/rollback.php
@@ -4,24 +4,10 @@ require_once __DIR__ . "/../../index.php";
 $db_name = $_ENV['DB_NAME'];
 $db_con = $database->connection();
 
-$db_con->exec(<<<CT
-DROP TABLE IF EXISTS Ceremony_Attendance;
-CT);
-
-$db_con->exec(<<<CT
-DROP TABLE IF EXISTS Clothes;
-CT);
-
-$db_con->exec(<<<CT
-DROP TABLE IF EXISTS Students;
-CT);
-
-$db_con->exec(<<<CT
-DROP TABLE IF EXISTS Users;
-CT);
-
-$db_con->exec(<<<CT
-DROP TABLE IF EXISTS Ceremony;
-CT);
+$db_con->exec('DROP TABLE IF EXISTS Ceremony_Attendance;');
+$db_con->exec('DROP TABLE IF EXISTS Clothes;');
+$db_con->exec('DROP TABLE IF EXISTS Students;');
+$db_con->exec('DROP TABLE IF EXISTS Users;');
+$db_con->exec('DROP TABLE IF EXISTS Ceremony;');
 
 ?>
